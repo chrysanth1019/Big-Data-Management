c508b0d233648be6535a5cd6c24e727092c7da90
chrysanth1019 <w.chrysanth1019@gmail.com>
2025-05-12 12:45:04 +0200

    test

--- app/Http/Controllers/SimpleSearchController.php
+++ app/Http/Controllers/SimpleSearchController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
-use App\Models\category;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Carbon;
