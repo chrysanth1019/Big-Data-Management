960937bcbe41c48e89d5157409a798aa06b10aa7
chrysanth1019 <w.chrysanth1019@gmail.com>
2025-05-12 12:46:26 +0200

    test

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -74,6 +74,7 @@
                                 ">
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div>
+                                            {{ dd($activity->user) }}
                                             <strong>{{ $activity->user->name }}</strong> 
                                             <span class="text-muted small">
                                                 @switch($activity->action)
